44146083d27f210f43e6f46b5db365d48130086a
Hari <handuleharun@gmmail.com>
2026-09-15 12:36:14 +0200

    Initial project setup

---
+++ src/main/java/com/example/smartdagligvare/SmartDagligVareApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartdagligvare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDagligVareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartDagligVareApplication.class, args);
+    }
+
+}
